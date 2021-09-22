85ab53689b2aef81b0284e763b63993da4560d61
ue-sho <tssu45@gmail.com>
2021-09-23 00:18:25 +0900

    feat: 今日以外のコミット数を取れるように変更

--- run.py
+++ run.py
@@ -1,27 +1,33 @@
-from slackbot.bot import Bot
-from slackbot.bot import respond_to
-from slackbot.bot import listen_to
-import requests
+import datetime
 
-from datetime import datetime
 import pytz
+import requests
+from github import Github
+from slackbot.bot import (
+    Bot,
+    respond_to,
+    listen_to
+)
+
 
 # GITHUB_ACCESS_TOKEN = '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'
+
+
 def iso_to_jstdt(iso_str):
     dt = None
     try:
-        dt = datetime.strptime(iso_str, '%Y-%m-%dT%H:%M:%SZ')
+        dt = datetime.datetime.strptime(iso_str, '%Y-%m-%dT%H:%M:%SZ')
         dt = pytz.utc.localize(dt).astimezone(pytz.timezone("Asia/Tokyo"))
     except ValueError:
         try:
-            dt = datetime.strptime(iso_str, '%Y-%m-%dT%H:%M:%S.%f%z')
+            dt = datetime.datetime.strptime(iso_str, '%Y-%m-%dT%H:%M:%S.%f%z')
             dt = dt.astimezone(pytz.timezone("Asia/Tokyo"))
         except ValueError:
             pass
     return dt
 
 
-def commit_count():
+def commit_count(day=0):
     commit_count = 0
 
     res = requests.get('https://api.github.com/users/ue-sho/events')
@@ -31,7 +37,9 @@ def commit_count():
             continue
 
         dt = iso_to_jstdt(data["created_at"])
-        if dt.strftime('%Y/%m/%d') != datetime.now().strftime('%Y/%m/%d'):
+        now = datetime.datetime.now()
+        yesterday = now - datetime.timedelta(days=day)
+        if dt.strftime('%Y/%m/%d') != yesterday.strftime('%Y/%m/%d'):
             continue
 
         comit_list = data["payload"]["commits"]
@@ -40,18 +48,24 @@ def commit_count():
 
     return commit_count
 
-def main():
-    commit_count()
 
+def main():
     bot = Bot()
     bot.run()
 
 
-
-@respond_to('commit_count')
-def test_respond_to(message):
+@respond_to('今日')
+def today_respond_to(message):
     res = commit_count()
     message.send("今日のコミット数は {} です".format(res))
+
+
+@respond_to(r'.*日前')
+def before_days_respond_to(message):
+    text = message.body['text']
+    day = int(text[:-2])
+    res = commit_count(day)
+    message.send("{}日前のコミット数は {} です".format(day, res))
 
 
 # @listen_to('prod')
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,5 +1,5 @@
 API_TOKEN = "xoxb-572457194997-2532056937649-kyDEzA3DOjFQGEK8JWWZln2y"
 
-DEFAULT_REPLY = "Git Statsです"
+DEFAULT_REPLY = "Git Statsです。「今日」 or 「○日前」 と入力してください。"
 
 PLUGINS = ['plugins']
